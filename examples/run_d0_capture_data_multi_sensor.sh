3a49fe15a6c7b0ce7bbf68e19d0f3112434b6603
spencer@primus <spencer.hallyburton@duke.edu>
2023-09-08 10:34:17 -0400

    Remove seed from data captures

--- examples/run_d0_capture_data_multi_sensor.sh
+++ examples/run_d0_capture_data_multi_sensor.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 N_SCENARIOS=${1:-20}
-MAX_SCENARIO_LEN=${2:-20}
+MAX_SCENARIO_LEN=${2:-40}
 
 source setup_for_standard.bash
 
@@ -10,5 +10,4 @@ python exec_standard.py \
 	--max_scenario_len $MAX_SCENARIO_LEN \
 	--config_avstack 'PassthroughAutopilotVehicle' \
 	--config_carla 'scenarios/capture_data_multi_sensor.yml' \
-	--image_dump_time 25 \
-	--seed 1
+	--image_dump_time 25
--- examples/run_d1_capture_data_multi_agent.sh
+++ examples/run_d1_capture_data_multi_agent.sh
@@ -10,5 +10,4 @@ python exec_standard.py \
     --max_scenario_len $MAX_SCENARIO_LEN \
     --config_avstack 'PassthroughAutopilotVehicle' \
     --config_carla 'scenarios/capture_data_multi_agent.yml' \
-    --image_dump_time 25 \
-    --seed 1
+    --image_dump_time 25
